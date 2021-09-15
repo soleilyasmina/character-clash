b683f0e466dcdad296c1df1d9152164c08a3a1c6
soleilyasmina <mounlasolomon@gmail.com>
2021-09-15 14:34:29 -0700

    - update input type number to be of type range - add state and listeners to each input

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,41 @@
+import { useState } from "react";
+
 function Form(props) {
+  const [name, setName] = useState("");
+  const [game, setGame] = useState("");
+  const [rating, setRating] = useState(1);
+
   return (
     <form>
       <label htmlFor="name">Name:</label>
-      <input id="name" type="text" />
+      <input
+        id="name"
+        type="text"
+        required
+        autoComplete="off"
+        autoFocus
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <label htmlFor="game">Game:</label>
-      <input id="game" type="text" />
-      <label htmlFor="rating">Rating:</label>
-      <input type="number" id="rating" />
+      <input
+        id="game"
+        type="text"
+        required
+        value={game}
+        onChange={(e) => setGame(e.target.value)}
+      />
+      <label htmlFor="rating">Rating: {rating}/10</label>
+      <input
+        type="range"
+        id="rating"
+        required
+        step={1}
+        min={1}
+        max={10}
+        value={rating}
+        onChange={(e) => setRating(e.target.valueAsNumber)}
+      />
       <button type="submit">Let Me Judge Them!</button>
     </form>
   );
